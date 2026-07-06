8e812bebfb56d8c1a0ea55fa570560b67008eb29
Adrian P. Anicete <adriana@PhilLife.com>
2026-07-06 14:01:25 +0800

    Add seed script for companies and admin accounts

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import config from './src/config/env.js';
 import { poolPromise } from './src/config/db.js';
+import router from './src/routes/authRoutes.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Beneficiary Enrollment');
 });
+
+app.use('/api/auth', adminRoutes)
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`));
