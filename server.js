1d3f04cd148c25f857c24287120c5b4b7ddfbfd5
Adrian P. Anicete <adriana@PhilLife.com>
2026-07-06 08:51:42 +0800

    update server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 7001
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
