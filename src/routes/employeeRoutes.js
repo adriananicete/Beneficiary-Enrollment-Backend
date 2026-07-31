de625d3fd57eff336e04e60fd2f859bd12fb5e02
Adrian P. Anicete <adriana@PhilLife.com>
2026-07-31 11:48:51 +0800

    fix: Rate Limiting on Auth & Enrollment Endpoints (Security #5, #8)

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -4,12 +4,13 @@ import { verifyToken } from '../middlewares/verifyToken.js';
 import { allowedRoles } from '../middlewares/allowedRoles.js';
 import { EMPLOYEE } from '../utils/constants.js';
 import { verifyResetToken } from '../middlewares/verifyResetToken.js';
+import { strictLimiter } from '../middlewares/rateLimiter.js';
 
 const router = express.Router();
 
-router.post('/login', login);
+router.post('/login', strictLimiter, login);
 router.post('/logout', logout);
-router.post('/change-password', verifyResetToken, changePassword);
+router.post('/change-password', strictLimiter, verifyResetToken, changePassword);
 router.get('/enrollment', verifyToken, allowedRoles(EMPLOYEE), getMyEnrollment);
 router.put('/enrollment', verifyToken, allowedRoles(EMPLOYEE), editMyEnrollment);
 
