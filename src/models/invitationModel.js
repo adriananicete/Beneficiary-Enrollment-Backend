b65aeb7478c015ef3bb4f4c00820a6d01d8839c9
Adrian P. Anicete <adriana@PhilLife.com>
2026-08-12 14:12:20 +0800

    feat: HR invitation list endpoint

--- src/models/invitationModel.js
+++ src/models/invitationModel.js
@@ -6,8 +6,17 @@ const getInvitationByToken = async (pool, token) => {
     .execute('usp_sel_enrollment_invitation_by_token');
 
     return result.recordset[0];
-}
+};
+
+const getInvitationsByUser = async (pool, userId) => {
+    const result = await pool.request()
+    .input('us01_user_id', sql.BigInt, userId)
+    .execute('usp_sel_enrollment_invitations_by_user')
+
+    return result.recordset;
+};
 
 export default {
     getInvitationByToken,
+    getInvitationsByUser,
 }
